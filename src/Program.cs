fab38a731e37ed361bd3ce19a74c3ae7b171386d
S J <444820@cognizant.com>
2017-07-04 17:56:52 +0530

    first checkin

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+
+
+using System;
+
+namespace MyFirstProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Writeline("Testing");
+        }
+    }
+}
